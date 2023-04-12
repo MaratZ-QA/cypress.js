612d904863077db16d723b07be29262061e6a520
Марат <127504791+MaratZ-QA@users.noreply.github.com>
2023-04-12 22:14:56 +0300

    Update HuntingPony.cy.js

--- cypress/e2e/HuntingPony.cy.js
+++ cypress/e2e/HuntingPony.cy.js
@@ -4,6 +4,7 @@ describe('Тестирование HuntingPony', function () {
         cy.visit('https://huntingpony.com/');
         cy.get('[data-index="4"] > .header__collections-controls > .header__collections-link').click();
         cy.get('[data-product-id="340983579"] > .product-preview__content > .product-preview__area-photo > .product-preview__photo > .img-ratio > .img-ratio__inner > a > :nth-child(1) > .product-preview__img-1').click();
+        cy.wait(3000);
         cy.get('.add-cart-counter__btn').click();
         cy.get('[data-add-cart-counter-plus=""]').click();
         cy.get('.header__cart').click();
